29611833f92c5e0393a3eaf91ce35cee737b9f68
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 05:16:41 +0530

    very big sum

---
+++ hacker-rank-solutions/ProblemSolving/VeryBigSum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ProblemSolving
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/a-very-big-sum/problem
+    /// </summary>
+    internal static class VeryBigSum
+    {
+        public static long Execute(List<long> ar)
+        {
+            return ar.Sum();
+        }
+    }
+}
