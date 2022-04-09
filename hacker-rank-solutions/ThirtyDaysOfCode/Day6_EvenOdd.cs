539749a4e9b09abac76a9f097a2e265848e6c65b
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 04:00:35 +0530

    day6 - even odd

---
+++ hacker-rank-solutions/ThirtyDaysOfCode/Day6_EvenOdd.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ThirtyDaysOfCode
+{
+    internal static class Day6_EvenOdd
+    {
+        static string Even { get; set; } = "";
+        static string Odd { get; set; } = "";
+
+        public static void Execute()
+        {
+            int count;
+            List<string> inputs = new List<string>();
+            List<string> outputs = new List<string>();
+
+            count = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1;
+                    i <= count;
+                    i++)
+            {
+                inputs.Add(Console.ReadLine());
+            }
+
+            foreach (string input in inputs)
+            {
+                EvenOdd(input);
+                Console.WriteLine($"{Even} {Odd}");
+            }
+        }
+
+        static void EvenOdd(string input)
+        {
+            Even = "";
+            Odd = "";
+
+            for (int i = 0;
+                    i < input.Length;
+                    i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Even += input[i].ToString();
+                }
+                else
+                {
+                    Odd += input[i].ToString();
+                }
+            }
+        }
+
+    }
+}
