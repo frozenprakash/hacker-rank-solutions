0d56f986a6475e43fe87ddefe803ba30f0571188
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 03:42:11 +0530

    day 3 - operators

--- hacker-rank-solutions/Program.cs
+++ hacker-rank-solutions/Program.cs
@@ -3,4 +3,7 @@ using hacker_rank_solutions.ThirtyDaysOfCode;
 
 // Enter the function below to be executed
 //Day0_HelloWorld.Execute();
-Day1_DataTypes.Execute();
+//Day1_DataTypes.Execute();
+//Day2_Operators.Execute(12.00,
+//                        20,
+//                        8);
---
+++ hacker-rank-solutions/ThirtyDaysOfCode/Day2_Operators.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ThirtyDaysOfCode
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/30-operators/problem
+    /// </summary>
+    internal static class Day2_Operators
+    {
+        public static void Execute(double meal_cost,
+                                   int tip_percent,
+                                   int tax_percent)
+        {
+            decimal meal,
+                    tip,
+                    tax,
+                    total;
+
+            meal = (decimal)meal_cost;
+            tip = meal * (tip_percent / 100m);
+            tax = meal * (tax_percent / 100m);
+
+            total = meal + tip + tax;
+            total = Math.Round(total,
+                               0);
+
+            Console.WriteLine(total);
+        }
+    }
+}
