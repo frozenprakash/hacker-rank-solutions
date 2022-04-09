7387620d4aed5bdd88fbf6be86b01b324fd25393
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 05:26:30 +0530

    birthday cake candles

---
+++ hacker-rank-solutions/ProblemSolving/BirthdayCakeCandles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ProblemSolving
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+    /// </summary>
+    internal static class BirthdayCakeCandles
+    {
+        public static int Exeucte(List<int> candles)
+        {
+            int max = candles.Max();
+            return candles.Where(x => x == max)
+                          .Count();
+        }
+    }
+}
