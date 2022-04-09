b787f002c6a380f5b3a6c4aacde1f3ebb733a100
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 04:02:16 +0530

    Day7 - Arrays

---
+++ hacker-rank-solutions/ThirtyDaysOfCode/Day7_Arrays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ThirtyDaysOfCode
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/30-arrays/problem
+    /// </summary>
+    internal static class Day7_Arrays
+    {
+        public static void Execute()
+        {
+            int n = Convert.ToInt32(Console.ReadLine()
+                        .Trim());
+            List<int> arr = Console.ReadLine()
+                                    .TrimEnd()
+                                    .Split(' ')
+                                    .ToList()
+                                    .Select(arrTemp => Convert.ToInt32(arrTemp))
+                                    .ToList();
+            arr.Reverse();
+            Console.WriteLine(string.Join(" ",
+                                                arr));
+        }
+    }
+}
