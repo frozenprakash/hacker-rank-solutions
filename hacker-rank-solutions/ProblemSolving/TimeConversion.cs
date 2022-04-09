d461a5da2041459233892564b7d59c8a7c0978dc
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 05:27:44 +0530

    time conversaion

---
+++ hacker-rank-solutions/ProblemSolving/TimeConversion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ProblemSolving
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/time-conversion/problem
+    /// </summary>
+    internal static class TimeConversion
+    {
+        public static string Execute(string s)
+        {
+            return DateTime.Parse(s)
+                           .ToString("HH:mm:ss");
+        }
+    }
+}
