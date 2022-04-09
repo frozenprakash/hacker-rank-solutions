11db9c654f20a838555ddab97f3ac6da509f5411
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 05:12:43 +0530

    git fix

---
+++ hacker-rank-solutions/ProblemSolving/CompareTheTriplets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ProblemSolving
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/compare-the-triplets/problem
+    /// </summary>
+    internal static class CompareTheTriplets
+    {
+        public static List<int> Execute(List<int> a,
+                                    List<int> b)
+        {
+            int alice = 0,
+            bob = 0;
+
+            for (int i = 0;
+                    i < a.Count;
+                    i++)
+            {
+                if (a[i] > b[i])
+                {
+                    alice++;
+                }
+                else if (b[i] > a[i])
+                {
+                    bob++;
+                }
+            }
+
+            return new List<int>() { alice, bob };
+        }
+    }
+}
---
+++ hacker-rank-solutions/ProblemSolving/SimpleArraySum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ProblemSolving
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/simple-array-sum/problem
+    /// </summary>
+    internal static class SimpleArraySum
+    {
+        public static int Execute(List<int> ar)
+        {
+            return ar.Sum();
+        }
+    }
+}
