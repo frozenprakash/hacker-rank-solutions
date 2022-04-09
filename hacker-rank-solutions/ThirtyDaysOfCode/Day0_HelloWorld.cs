200932f9c3502c65c78fcb55e07c1eb882434c60
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 03:18:09 +0530

    day 0 - hello world

---
+++ hacker-rank-solutions/ThirtyDaysOfCode/Day0_HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ThirtyDaysOfCode
+{
+    internal class Day0_HelloWorld
+    {
+        static void Execute()
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello, World.");
+            Console.WriteLine(input);
+        }
+    }
+}
