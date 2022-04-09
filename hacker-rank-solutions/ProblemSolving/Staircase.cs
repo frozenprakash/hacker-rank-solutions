c8b8a2b143fcd6208f844d76694057a84c3f17ec
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 05:23:16 +0530

    cosmetic

---
+++ hacker-rank-solutions/ProblemSolving/Staircase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ProblemSolving
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/staircase/problem
+    /// </summary>
+    internal static class Staircase
+    {
+        public static void Execute(int n)
+        {
+            string space = "",
+                    hash = "";
+
+            for(int i = 1;
+                    i <= n;
+                    i++)
+            {
+                space = string.Concat(Enumerable.Repeat(" ",
+                                                                n - i));
+                hash = string.Concat(Enumerable.Repeat("#", 
+                                                                i));
+                Console.WriteLine(space + hash);
+            }
+        }
+    }
+}
