ec905fc67e9fd1f5a5094dd0493c9b5af52910a2
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 05:37:13 +0530

    Counting valleys and mountains

---
+++ hacker-rank-solutions/ProblemSolving/CountingValleysAndMountains.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ProblemSolving
+{
+    internal static class CountingValleysAndMountains
+    {
+        public static int countingValleys(int steps,
+                                          string path)
+        {
+            int prevStep = 0,
+                step = 0,
+                mountain = 0,
+                valley = 0;
+
+            foreach (char c in path)
+            {
+                switch (c)
+                {
+                    case 'U':
+                        step++;
+                        if (prevStep == 0)
+                        {
+                            mountain++;
+                        }
+
+                        break;
+
+                    case 'D':
+                        step--;
+                        if (prevStep == 0)
+                        {
+                            valley++;
+                        }
+
+                        break;
+                }
+
+                prevStep = step;
+            }
+
+            return valley;
+            //return mountain;
+        }
+    }
+}
