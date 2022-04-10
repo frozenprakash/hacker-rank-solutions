fe67846f181c7626153491e0e4e771c74f8edf81
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 05:41:51 +0530

    Jumping on cloud

---
+++ hacker-rank-solutions/ProblemSolving/JumpingOnCloud.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ProblemSolving
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+    /// </summary>
+    internal static class JumpingOnCloud
+    {
+        public static int Execute(List<int> c)
+        {
+            int jump = 0;
+
+            for(int i = 0;
+                    i < c.Count;
+                    i++)
+            {
+                if (i + 2 < c.Count &&
+                    c[i + 2] == 0)
+                {
+                    jump++;
+                    i += 1;
+                }
+                else if (i + 1 < c.Count &&
+                            c[i + 1] == 0)
+                {
+                    jump++;
+                }
+            }
+
+            return jump;
+        }
+    }
+}
