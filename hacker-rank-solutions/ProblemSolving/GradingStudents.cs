3abb5e040686cf5e522bcbf8d11edfee5489aaa5
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-04-10 05:30:23 +0530

    grading students

---
+++ hacker-rank-solutions/ProblemSolving/GradingStudents.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank_solutions.ProblemSolving
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/grading/problem
+    /// </summary>
+    internal static class GradingStudents
+    {
+        public static List<int> Execute(List<int> grades)
+        {
+            int grade,
+            nextMultiple;
+            List<int> newGrades = new List<int>();
+
+            for(int i = 0;
+                    i < grades.Count;
+                    i++)
+            {
+                grade = grades[i];
+                nextMultiple = ((grade / 5) + 1) * 5;
+
+                if (grade >= 38 &&
+                    nextMultiple - grade < 3)
+                {
+                    newGrades.Add(nextMultiple);
+                }
+                else
+                {
+                    newGrades.Add(grade);
+                }
+            }
+
+            return newGrades;
+        }
+    }
+}
