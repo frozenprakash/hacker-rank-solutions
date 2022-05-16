c598dcea7f5492c6f51c047d7200fa44757489fc
Chandraprakash Ganesan <Chandraprakash_Ganesan@mckinsey.com>
2022-05-17 03:44:03 +0530

    Regex - Split the phone numbers

---
+++ hacker-rank-solutions/RegExp/SplitThePhoneNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace hacker_rank_solutions.RegExp
+{
+    internal static class SplitThePhoneNumbers
+    {
+        static void Main(String[] args)
+        {
+            List<string> inputs;
+            Match m;
+
+            inputs = GetInputs();
+            foreach (string input in inputs)
+            {
+                m = Regex.Match(input,
+                                @"^(.*?)[-| ](.*?)[-| ](.*?)$");
+                Console.WriteLine($"CountryCode={m.Groups[1].Value},LocalAreaCode={m.Groups[2].Value},Number={m.Groups[3].Value}");
+            }
+        }
+
+        static List<string> GetInputs()
+        {
+            int count;
+            List<string> inputs;
+
+            count = Convert.ToInt32(Console.ReadLine());
+            inputs = new List<string>();
+
+            for (int i = 1;
+                    i <= count;
+                    i++)
+            {
+                inputs.Add(Console.ReadLine());
+            }
+
+            return inputs;
+        }
+    }
+}
